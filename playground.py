70eaf7fa95cf6e27471a221e6c794338aa7004a4
jippey67 <jeroenwalta@gmail.com>
2019-09-20 18:26:54 +0200

    added min and max values

--- playground.py
+++ playground.py
@@ -4,18 +4,23 @@ import keys
 import ssl
 import urllib.request
 import json
-client = Client(keys.api_key, keys.api_secret, {"verify": False, "timeout": 20})
+
+
+#global minPrice, maxPrice
+minPrice = 1
+maxPrice = 1
 
 def process_message(msg):
-
-
+    global minPrice
+    global maxPrice
     usdt = float(msg.get('p'))
     bitcoin = json.loads(urllib.request.urlopen(url, context=context).read()).get("USD")
     calc = bitcoin/usdt
-    print('1 USDT is', calc, 'USD. BTC:', bitcoin, 'USDT:', usdt)
+    minPrice = min(minPrice, calc)
+    maxPrice = max(maxPrice, calc)
+    print('1 USDT is', '{:10.8f}'.format(calc), 'max is', '{:6.4f}'.format((maxPrice-1)*100),'% min is', '{:6.4f}'.format((minPrice-1)*100), '%')
 
-    #print(msg)
-    # do something
+client = Client(keys.api_key, keys.api_secret, {"verify": False, "timeout": 20})
 
 context = ssl._create_unverified_context()
 url = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD&api_key={3d61edda26c698f29ca05ad2e6f5f5dccc10b2b25a41fce0bd9eafa951166110}'
