690978e2e9b950f0b7d797034278f060299f1326
jippey67 <jeroenwalta@gmail.com>
2019-10-04 17:34:41 +0200

    added test orders and logging

--- engine.py
+++ engine.py
@@ -1,24 +1,44 @@
 from binance.websockets import BinanceSocketManager
 from binance.client import Client
-from binance.enums import *
 
 import ssl
 import urllib.request
 import json
 import datetime
+import urllib3
+
 
 import mailer
-import specifications
+from simplecrypt import decrypt
+from getpass import getpass
 
 debug = True
+test = True
+production = not test
+urllib3.disable_warnings()
 
-triggerHi = 1 + specifications.targetTrigger
-triggerLo = 1 - specifications.targetTrigger
-orderSpace = specifications.orderSpace
+with open('specs.cfg', 'rb') as file:
+    specsString = file.read()
+pw = getpass()
+unencodedString = decrypt(pw, specsString)
+specifications = eval(unencodedString)
+
+now = datetime.datetime.now()
+dt_string = now.strftime("%d-%m-%Y_%H:%M:%S")
+quoteLog = 'quotes'+dt_string+'.csv'
+
+if test:
+    triggerHi = 1 + 0.001
+    triggerLo = 1 - 0.001 
+    orderSpace = specifications['orderSpace']
+if production:
+    triggerHi = 1 + specifications['targetTrigger']
+    triggerLo = 1 - specifications['targetTrigger']
+    orderSpace = specifications['orderSpace']
 
 class Robot:
     def __init__(self):
-        self.client = Client(specifications.api_key_p, specifications.api_secret_p, {"verify": False, "timeout": 20})
+        self.client = Client(specifications['api_key_p'], specifications['api_secret_p'], {"verify": False, "timeout": 20})
         self.bm = BinanceSocketManager(self.client)
         self.bm.start_trade_socket('BTCUSDT', self.market_event)
         self.bm.start_user_socket(self.user_event)
@@ -39,11 +59,12 @@ class Robot:
 
         self.orderStatus = ''
         self.orderId = ''
-        self.NoOrderInProgress = True  # prevent multiple orders due to exchange latency
+        self.noOrderInProgress = True  # prevent multiple orders due to exchange latency
+        self.nextPosition = ''
 
     def getBTCquote(self, oldQuote):
         context = ssl._create_unverified_context()
-        url = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD&api_key={3d61edda26c698f29ca05ad2e6f5f5dccc10b2b25a41fce0bd9eafa951166110}'
+        url = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD&api_key=' + specifications['api_key_btc']
         try:
             btcusd = json.loads(urllib.request.urlopen(url, context=context).read()).get("USD")
             return btcusd
@@ -75,43 +96,66 @@ class Robot:
         if msg['e'] == 'executionReport':
             if msg['x'] == 'TRADE':
                 self.logOrders('order filled: '+msg['i'])
-                self.NoOrderInProgress = True
-                mailer.mailer('order filled: '+msg['i'])
+                self.noOrderInProgress = True
+                mailer.mailer(specifications['mail_a'], specifications['mail_p'], 'order filled: '+msg['i'])
+                self.position = self.nextPosition
+
+    def testOrder(self, rate, action, tetP, btcP, limitPrice, quantity, nextPosition):
+        self.position = nextPosition
+        logString = action + ' ' + str(rate) + ', tether: ' + str(tetP) + ', bitcoin: ' +str(btcP) + ', limit: ' + str(limitPrice) + ', quantity: ' + str(quantity) + ', position: ' + nextPosition
+        self.logOrders(logString)
+        self.noOrderInProgress = True
 
     def market_event(self, msg):
         usdt = float(msg.get('p'))
         self.bitcoin = self.getBTCquote(self.bitcoin)
         rate = self.bitcoin / usdt
-        if debug: print('1 USDT is', '{:10.8f}'.format(rate))
-        if self.NoOrderInProgress:
+        if self.noOrderInProgress:
             if self.position == 'FLAT':
                 if rate > triggerHi:
                     limitPrice = self.bitcoin*(1+orderSpace)
                     quantity = float(self.client.get_asset_balance(asset='BTC')['free'])
                     self.NoOrderInProgress = False
-                    self.order(['buy', quantity, limitPrice])
-                if rate > triggerLo:
+                    self.nextPosition = 'BITCOIN'
+                    buySell = 'buy'
+                    if production: self.order([buySell, quantity, limitPrice])
+                    if test: self.testOrder(rate, buySell, usdt, self.bitcoin, limitPrice, quantity, self.nextPosition)
+                if rate < triggerLo:
                     limitPrice = self.bitcoin * (1 - orderSpace)
                     quantity = float(self.client.get_asset_balance(asset='USDT')['free'])/limitPrice
                     self.NoOrderInProgress = False
-                    self.order(['sell', quantity, limitPrice])
+                    self.nextPosition = 'TETHER'
+                    buySell = 'sell'
+                    if production: self.order([buySell, quantity, limitPrice])
+                    if test: self.testOrder(rate, buySell, usdt, self.bitcoin, limitPrice, quantity, self.nextPosition)
             if self.position == 'BITCOIN':
                 if rate < 1:
                     limitPrice = self.bitcoin * (1 - orderSpace)
                     quantity = 0.5 * float(self.client.get_asset_balance(asset='BTC')['free'])
                     self.NoOrderInProgress = False
-                    self.order(['sell', quantity, limitPrice])
+                    self.nextPosition = 'FLAT'
+                    buySell = 'sell'
+                    if production: self.order([buySell, quantity, limitPrice])
+                    if test: self.testOrder(rate, buySell, usdt, self.bitcoin, limitPrice, quantity, self.nextPosition)
             if self.position == 'TETHER':
                 if rate > 1:
                     limitPrice = self.bitcoin * (1 + orderSpace)
                     quantity = 0.5 * float(self.client.get_asset_balance(asset='USDT')['free'])/limitPrice
                     self.NoOrderInProgress = False
-                    self.order(['buy', quantity, limitPrice])
-
+                    self.nextPosition = 'FLAT'
+                    buySell = 'buy'
+                    if production: self.order([buySell, quantity, limitPrice])
+                    if test: self.testOrder(rate, buySell, usdt, self.bitcoin, limitPrice, quantity, self.nextPosition)
+        if debug: print('1 USDT is', '{:10.8f}'.format(rate), self.position)
+        if test: self.logQuotes(rate)
 
     def logOrders(self, payload):
         with open("ordersLog.txt","a+") as file:
             file.write(str(datetime.datetime.utcnow())+" "+str(payload)+"\n")
+
+    def logQuotes(self, payload):
+        with open(quoteLog, 'a') as file:
+            file.write(str(datetime.datetime.utcnow())+","+str(payload)+"\n")
 
 bot = Robot()
 
--- mailer.py
+++ mailer.py
@@ -1,10 +1,9 @@
 import yagmail
-import specifications
 
-def mailer(payload):
+def mailer(address, passw, payload):
     address1 = "jeroenwalta@gmail.com"
     address2 = "jeroen@optieclub.nl"
     addressList = [address1, address2]
-    yag = yagmail.SMTP(specifications.mail_a, specifications.mail_p)
+    yag = yagmail.SMTP(address, passw)
     yag.send(to=addressList, subject='order filled', contents=payload)
 
